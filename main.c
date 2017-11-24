47e1f71c1f15adcbdf7ef0eb7c506aabdcf909bc
nari10 <ri7070@naver.com>
2017-11-24 21:30:14 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <curses.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "funin.h"
 
+int main()
+{
+	char ch;
+
+	Init();
+	signal(SIGALRM, sig_alrm);
+	set_ticker(500);
+
+	while(1) {
+	key_ctl();
+	}
+	endwin();
+	return 0;
+}
