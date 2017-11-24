113bfcd2e458c7af6feef56fd12c010e5063763d
nari10 <ri7070@naver.com>
2017-11-24 21:30:57 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+/*
+************************************
+* wsad控制上下左右，q退出，空格暂停
+* 编译命令 gcc snake.c -lcurses
+* 可能需要下载ncurses库，具体方法请百度
+* 发现bug请在贴吧@微笑一日
+*************************************
+*/
+/*
+*************************************
+*how to play: w:up, s:down, a:left, d:right, q:quit, space:pause
+*compile : gcc snake.c -lcurses
+*you may need curses library,please google it
+*if you find bugs,mail me at weixiaoyiri@gmali.com
+*************************************
+*/
+
 #include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
